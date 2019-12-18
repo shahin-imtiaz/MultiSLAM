e3c0bc3f4032e24b25cd4193f6004f1a36134667
Shahin Imtiaz <shahin.imtiaz@mail.utoronto.ca>
2019-12-18 16:45:15 -0500

    Add a few more improvements to setup script to make it work start to end.

--- mslam_setup_cdf.sh
+++ mslam_setup_cdf.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+set -eu -o pipefail
+
 echo "Installing mslam. This computer must have CUDA enabled support"
-export CUDA_HOME='/usr/local/cuda-10.0/'
-python3 -m venv env-mslam
+export CUDA_HOME="${CUDA_HOME:-/usr/local/cuda-10.0/}"
+python3 -m venv --system-site-packages env-mslam
 source env-mslam/bin/activate
-python3 -m pip install -U pip
-python3 -m pip install -r requirements.pip
+alias pip="python3 -m pip"
+pip install -U pip
+pip install -r requirements.pip
 git clone https://github.com/facebookresearch/detectron2 detectron2_repo
 git clone https://github.com/nianticlabs/monodepth2.git
 git clone https://github.com/ucbdrive/hd3.git
-python3 -m pip install -U 'git+https://github.com/facebookresearch/fvcore.git'
-python3 -m pip install -U 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
-python3 -m pip install -e detectron2_repo
-python3 -m pip install -U "git+https://github.com/cocodataset/panopticapi.git"
-python3 -m pip install -U "git+https://github.com/lvis-dataset/lvis-api.git"
-python3 -m pip install -U "git+https://github.com/mcordts/cityscapesScripts.git"
-python3 -m pip install -r requirements.pip
+pip install Cython cupy-cuda100
+pip install -U 'git+https://github.com/facebookresearch/fvcore.git'
+pip install -U 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
+pip install -e detectron2_repo
+pip install -U "git+https://github.com/cocodataset/panopticapi.git"
+pip install -U "git+https://github.com/lvis-dataset/lvis-api.git"
+pip install -U "git+https://github.com/mcordts/cityscapesScripts.git"
+pip install -r requirements.pip
 cd monodepth2_repo && python3 test_simple.py --image_path assets/test_image.jpg --model_name mono_1024x320
 cd ../
 echo "Done setup. Please run: python3 mslam.py"
